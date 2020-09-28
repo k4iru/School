4d70aab45b8180cd66b4b3aca83c30dd6ba9f702
Kyle Cheung <kcheu4@uwo.ca>
2020-09-27 21:05:11 -0400

    finsihed assignment

--- HTTP-5103/Assignment-2-GroupLogin/script.js
+++ HTTP-5103/Assignment-2-GroupLogin/script.js
@@ -1,21 +1,31 @@
-var groupList = {
-    "kyle": "cheung",
-    "muhammad": "effendi",
-    "ikumi": "mine",
-    "anetor": "irenen",
-    "simranjeet": "singh"
-};
-
 let userGroup = prompt("What Group are you in?");
 
 if (userGroup === "3B" || userGroup === "3b") {
     let firstName = prompt("whats your first name?");
 
-    if (firstName.toLowerCase() in groupList) {
-        console.log(firstName);
-        alert(`Welcome back ${firstName} ${groupList[firstName]}`);
-    } else {
-        alert("Invalid Name: Access Denied");
+    switch (firstName) {
+        case "kyle":
+        case "Kyle":
+            alert("Welcome Back Kyle Cheung");
+            break;
+        case "muhammad":
+        case "Muhammad":
+            alert("Welcome Back Muhammad Effendi");
+            break;
+        case "ikumi":
+        case "Ikumi":
+            alert("Welcome Back Ikumi Mine");
+            break;
+        case "anetor":
+        case "Anetor":
+            alert("Welcome Back Anetor Irenen");
+            break;
+        case "simranjeet":
+        case "Simranjeet":
+            alert("Welcome Back Simranjeet Singh");
+            break;
+        default:
+            alert("Invalid Input: Access Denied");
     }
 } else {
     alert("Access Denied");
