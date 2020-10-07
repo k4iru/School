c479610265efb17134a95c9831b3b43cf4bdaf07
Kyle Cheung <kcheu4@uwo.ca>
2020-10-07 12:48:42 -0400

    labs

--- HTTP-5105/lab4.sql
+++ HTTP-5105/lab4.sql
@@ -1,7 +1,48 @@
 -- 1.
-SELECT withdrawal_id, first_name, last_name
+Select w.*,
+    first_name,
+    last_name
+FROM withdrawals w
+    JOIN members m ON w.member_id = m.member_id -- 2.
+SELECT title,
+    first_name,
+    last_name,
+    w.*
 FROM withdrawals
-    JOIN members
-    ON withdrawals.member_id = members.member_id 
-    
--- 2.
+    JOIN members m ON w.member_id = m.member_id
+    JOIN books b ON w.book = b.book_id -- 3.
+SELECT title,
+    first_name,
+    last_name,
+    w.*
+FROM withdrawals
+    JOIN members m ON w.member_id = m.member_id
+    JOIN books b ON w.book = b.book_id
+ORDER BY withdrawal_date DESC -- 4. 
+SELECT title,
+    COUNT(*)
+FROM withdrawals w
+    JOIN books b ON w.book_id = b.book_id
+GROUP BY title
+ORDER BY COUNT(*) DESC
+LIMIT 5 -- 5.
+SELECT title,
+    first_name,
+    last_name
+FROM books b
+    JOIN authorship a ON b.book_id = a.book_id
+    JOIN authors a2 ON a.author_id = a2.author_id -- 6.
+SELECT title,
+    first_name,
+    last_name
+FROM books b
+    JOIN authorship a ON b.book_id = a.book_id
+    JOIN authors a2 ON a.author_id = a2.author_id
+WHERE genre = 'Horror' -- 7. 
+SELECT title,
+    first_name,
+    last_name
+FROM books b
+    JOIN authorship a ON b.book_id = a.book_id
+    JOIN authors a2 ON a.author_id = a2.author_id
+WHERE title LIKE 'Night of the Living Dummy%'
