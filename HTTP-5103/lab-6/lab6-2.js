891277a56b602b459863b8dfff75e6891f87bd46
Kyle Cheung <kcheu4@uwo.ca>
2020-10-13 14:46:06 -0400

    lab 6

---
+++ HTTP-5103/lab-6/lab6-2.js
@@ -0,0 +1,38 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+//alert("test");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
+
+//LISTEN FOR load EVENT
+window.onload = init;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function init() {
+	//GET DOM ELEMENTS WE'LL NEED
+	let mysteryBox = document.getElementById("mysteryBox");
+	let buttonBox = document.getElementById("buttonBox");
+	//====CREATE THE FUNCTIONS WE'LL NEED====
+	//FUNCTION TO ASK USER
+	function mysteryBoxFunc() {
+		let userChoice = confirm("Do you want to see somehing?");
+
+		if (userChoice) {
+			mysteryBox.style.display = "none";
+			buttonBox.style.display = "block";
+
+		}
+	}
+
+	//FUNCTION TO CHANGE buttonBox
+	function buttonBoxFunc() {
+		buttonBox.style.width = 600 + "px";
+		buttonBox.innerHTML = "<h2>SURPRISE</h2>";
+		buttonBox.style.background = "orange";
+	}
+
+	//SETUP LISTENERS
+	mysteryBox.onmouseover = mysteryBoxFunc;
+	buttonBox.onclick = buttonBoxFunc;
+
+
+	//END onload FUNCTION
+}
