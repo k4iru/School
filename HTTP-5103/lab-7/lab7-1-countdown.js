f71a4410452ce9da722c1a6d4eef631a7fdd9fda
Kyle Cheung <kcheu4@uwo.ca>
2020-11-10 15:20:22 -0500

    lab 7

--- HTTP-5103/lab-7/lab7-1-countdown.js
+++ HTTP-5103/lab-7/lab7-1-countdown.js
@@ -1,18 +1,37 @@
 ﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
-alert("Lab 7 - 1");//Please delete once you have confirmed your page is connected...seriously, if I see this popup when I check out your work, I will go coo-coo bananas.
+//alert("Lab 7 - 1");//Please delete once you have confirmed your page is connected...seriously, if I see this popup when I check out your work, I will go coo-coo bananas.
 
 //create page load listener
+window.onload = init;
 
 //create page load function
+function init() {
 
 	//create variables for required HTML elements
-	
+	let todayData = document.getElementById('todayData');
+	let finalData = document.getElementById('finalData');
+	let dueData = document.getElementById('dueData');
+
 	//create variables for now date and due date
-	
+	let today = new Date();
+	let dueDate = new Date('December 15, 2019 23:59');
+
 	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	let days = dueDate.getTime() - today.getTime();
 	
 	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
-	
+	let one_day = 1000 * 60 * 60 * 24;
+	let whole_days = Math.floor(days / one_day);
+	console.log(whole_days);
 	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	todayData.innerText = today.toDateString();
+	finalData.innerText = dueDate.toDateString();
 	
 	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+	if (days < 0) {
+		dueData.innerHTML = "The Deadline for the Final Assignment has passed!";
+	}
+	else {
+		dueData.innerText = whole_days;
+	}
+}
--- HTTP-5103/lab-7/lab7-2-clock.js
+++ HTTP-5103/lab-7/lab7-2-clock.js
@@ -1,22 +1,54 @@
 ﻿/* LAB 7.2 - STOP TIME */
-alert("Lab 2 is connected");//delete once confirming your page is connected. 
+//alert("Lab 2 is connected");//delete once confirming your page is connected. 
 
 //create page load listener
-
+window.onload = init;
 //create page load function
-
+function init() {
 	//create variables for required HTML elements
-	
+	let hoursOut = document.getElementById('hoursOut');
+	let minsOut = document.getElementById('minsOut');
+	let secsOut = document.getElementById('secsOut');
+	let btnStart = document.getElementById('btnStart');
+	let btnStop = document.getElementById('btnStop');
+
 	//create time variable so all functions have access to it
+	let time;
+	let hours;
+	let minutes;
+	let seconds;
 	
-	
+
 	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function displayTime() {
+		let date = new Date();
+
+		hours = date.getHours();
+		minutes = date.getMinutes();
+		seconds = date.getSeconds();
 	
+		hoursOut.innerText = format(hours % 12) +":";
+		minsOut.innerText = format(minutes) +":";
+		secsOut.innerText = format(seconds);
+	}
 	
 	//CREATE FUNCTION TO START THE CLOCK.
+	function startClock() {
+		time = setInterval(displayTime, 100);
 	
+	}
 	
 	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock() {
+		clearInterval(time);
+	}
 	
+	function format (n) {
+		let fn = ("0" + n).slice(-2);
+		return fn
+	  }
 	
 	// SET EVENT LISTENERS
+	btnStart.onclick = function() { startClock()};
+	btnStop.onclick = function() { stopClock()};
+}
