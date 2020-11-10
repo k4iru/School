96a5b5ffc45de3c2df55d20a49e6fb536054d7fa
Kyle Cheung <kcheu4@uwo.ca>
2020-11-10 14:08:58 -0500

    lab 7

---
+++ HTTP-5103/lab-7/lab7-1-countdown.js
@@ -0,0 +1,18 @@
+﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
+alert("Lab 7 - 1");//Please delete once you have confirmed your page is connected...seriously, if I see this popup when I check out your work, I will go coo-coo bananas.
+
+//create page load listener
+
+//create page load function
+
+	//create variables for required HTML elements
+	
+	//create variables for now date and due date
+	
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+	
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
---
+++ HTTP-5103/lab-7/lab7-2-clock.js
@@ -0,0 +1,22 @@
+﻿/* LAB 7.2 - STOP TIME */
+alert("Lab 2 is connected");//delete once confirming your page is connected. 
+
+//create page load listener
+
+//create page load function
+
+	//create variables for required HTML elements
+	
+	//create time variable so all functions have access to it
+	
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	
+	
+	//CREATE FUNCTION TO START THE CLOCK.
+	
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	
+	
+	// SET EVENT LISTENERS
