13c09b393863b4dc4749748da5effec5b9749516
Kyle Cheung <kcheu4@uwo.ca>
2020-11-25 22:06:39 -0500

    lab 9

--- HTTP-5103/lab-9/lab9-1-jquery.js
+++ HTTP-5103/lab-9/lab9-1-jquery.js
@@ -1,2 +1,19 @@
 //LAB 9 - 1 FAQ PAGE
-alert("1 - connected");
+
+// wait for DOM to load
+$(document).ready(function() {
+    // hide all paragraphs initially
+    $('p').hide();
+
+    // toggle display: block/none with 3000ms transition time
+    $('h2').click(function() {
+        $(this).next('p').slideToggle(3000);
+    });
+
+    // invert colours when mouse over paragraphs
+    $('p').hover(
+        function(){
+            $(this).toggleClass('textHovered');
+        }
+    );
+});
--- HTTP-5103/lab-9/lab9-2-jquery.js
+++ HTTP-5103/lab-9/lab9-2-jquery.js
@@ -1,2 +1,16 @@
 //LAB 9 - 2 INVENTORY PAGE
-alert("2 - connected");
+$(document).ready(function () {
+
+    // hide descriptions initially
+    $('td:nth-child(4)').hide();
+
+    // on hover selected row turns red
+    $('tr').hover(function() {
+        $(this).toggleClass('selected');
+    })
+
+    // on click toggle display of description
+    $('tr').click(function() {
+        $(this).find(':nth-child(4)').toggle();
+    })
+});
