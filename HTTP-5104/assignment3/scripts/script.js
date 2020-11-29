40bfdc01a0fbd8f9f43ff800bb8d620ea1b0a016
Kyle Cheung <kcheu4@uwo.ca>
2020-11-29 16:31:15 -0500

    section 1 and header

--- HTTP-5104/assignment3/scripts/script.js
+++ HTTP-5104/assignment3/scripts/script.js
@@ -1,3 +1,18 @@
+// https://www.w3schools.com/howto/howto_js_active_element.asp
+// https://stackoverflow.com/questions/52025615/vanilla-js-change-active-state-of-links-when-scrolling-refactoring
 window.onload = function() {
+    let navContainer = document.getElementById('main-nav');
+    let navLinks = navContainer.getElementsByClassName('nav-item');
 
-};
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', function() {
+            let current = document.getElementsByClassName('active');
+
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace('active', '');
+            }
+            this.className += ' active';
+            
+        });
+    }
+}
