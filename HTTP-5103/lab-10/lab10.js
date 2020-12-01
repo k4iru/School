e7846a4969be8a908abff5e19752dbfe9468c237
Kyle Cheung <kcheu4@uwo.ca>
2020-12-01 17:48:40 -0500

    finished lab 10

--- HTTP-5103/lab-10/lab10.js
+++ HTTP-5103/lab-10/lab10.js
@@ -1,2 +1,46 @@
 ﻿//LAB 10-DATA STORAGE: INDEX PAGE
-alert("home");//DELETE AFTER CONFIRMING CONNECTION
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+window.onload = function () {
+
+    if (document.cookie.split(';').some((item) => item.trim().startsWith('color='))) {
+        document.body.style.background = getCookie('color');
+    }
+
+    if (document.cookie.split(';').some((item) => item.trim().startsWith('name='))) {
+        let welcome = document.getElementById('newMsgBox');
+        welcome.innerText = `Welcome ${getCookie('name')}!`;
+    }
+    document.getElementById('btnDel').addEventListener("click", deleteCookie);
+
+};
+
+function deleteCookie() {
+    document.cookie = "name=;max-age=0";
+    document.cookie = "color=;max-age=0";
+    document.body.style.background = '#c0c0c0';
+    let welcome = document.getElementById('newMsgBox');
+    welcome.innerText = `Welcome!`;
+}
+
+function handleForm() {
+    let form = document.forms.infoForm;
+    let name = form.f_name.value;
+    let color = form.f_color.value;
+    makeCookie('name', name);
+    makeCookie('color', color);
+    document.body.style.background = getCookie('color');
+    let welcome = document.getElementById('newMsgBox');
+    welcome.innerText = `Welcome ${getCookie('name')}!`;
+    return false;
+}
+function makeCookie(name, value) {
+    document.cookie = `${name}=${value};max-age=${60 * 60 * 24}`;
+}
+
+function getCookie(name) {
+    let cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith(name))
+        .split('=')[1];
+    return cookieValue;
+}
--- HTTP-5103/lab-10/products.js
+++ HTTP-5103/lab-10/products.js
@@ -1,2 +1,19 @@
 ﻿//LAB 10-DATA STORAGE: PRODUCTS PAGE
-alert("products");//DELETE AFTER CONFIRMING CONNECTION
+window.onload = function() {
+    if (document.cookie.split(';').some((item) => item.trim().startsWith('color='))) {
+        document.body.style.background = getCookie('color');
+    }
+
+    if (document.cookie.split(';').some((item) => item.trim().startsWith('name='))) {
+        let welcome = document.getElementById('newMsgBox');
+        welcome.innerText = `Welcome ${getCookie('name')}!`;
+    }
+}
+
+function getCookie(name) {
+    let cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith(name))
+        .split('=')[1];
+    return cookieValue;
+}
