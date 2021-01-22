deab569de28a5e77115851ea147b3626c4e3a6a9
Kyle Cheung <kcheu4@uwo.ca>
2021-01-22 11:39:53 -0500

    5201  lab 1

---
+++ HTTP-5201/Lab-1-ConstructorLocation-2021/LAB1-1-routing.js
@@ -0,0 +1,18 @@
+//LAB 1-1: LOCATION OBJECT & ROUTING
+alert("Lab 1 - 1");//DELETE ONCE CONNECTED.
+
+//#### CONTENT FOR HOME PAGE ==== - BACKGROUND IS white
+var contentHome = "<h3>Welcome to our website!</h3>";
+contentHome += "<p>We have many wonderful products that you might be interested in.</p>";
+//==== END OF CONTENT FOR HOME PAGE ####
+
+//#### CONTENT FOR PRODUCTS PAGE ====  - BACKGROUND IS red
+var contentProducts = "<h3>Our Top 5 Products</h3>";
+contentProducts += "<ol><li>Computers</li><li>Hard Drives</li><li>USB Drives</li><li>Backup Drives</li><li>Printers</li></ol>";
+//==== END OF CONTENT FOR PRODUCTS PAGE #### 
+
+//#### CONTENT FOR ABOUT US PAGE ==== - BACKGROUND IS green
+var contentAbout = "<h3>About Computer Corp</h3>";
+contentAbout += "<p>We have been around a long time, so we know what we're doing.</p>";
+contentAbout += "<p><h4>Customer Feedback</h4><q>You're the only computer company I trust</q>&mdash;Ernst Blofeld</p>";
+//==== END OF CONTENT FOR ABOUT US PAGE ####
---
+++ HTTP-5201/Lab-1-ConstructorLocation-2021/LAB1-2-Fave3.js
@@ -0,0 +1,10 @@
+﻿/*LAB 1-2: CONSTRUCTOR FUNCTIONS*/
+//FAVE FRIEND CONSTRUCTOR FUNCTION
+alert("fave 3");
+
+window.onload = function(){
+	var faveThree = [];//FAVE 3 ARRAY
+
+
+
+}//END OF onload FUNCTION
