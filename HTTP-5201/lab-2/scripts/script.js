50e94fb98fcabcd0754e99917aa84ba326c21c92
Kyle Cheung <kcheu4@uwo.ca>
2021-02-01 00:46:58 -0500

    finished lab 2

--- HTTP-5201/lab-2/scripts/script.js
+++ HTTP-5201/lab-2/scripts/script.js
@@ -1,8 +1,42 @@
-"use strict";
+let form = document.forms[0];
+let loginForm = document.getElementById("login-form");
+let usernameInput = document.getElementById("username");
+let passwordInput = document.getElementById("password");
+let output = document.getElementById("output");
+let uOut = document.getElementById("uOut");
+let pOut = document.getElementById("pOut");
 
-function test() {
+form.onsubmit = function () {
+    "use strict";
+    let username = form.elements.usernamevalue;
+    let password = form.elements.password.value;
 
-    return "hello";
-}
 
-test();
+    // username validation
+    if (username === "") {
+        usernameInput.classList.add("form__label_invalid");
+    } else {
+        usernameInput.classList.remove("form__label_invalid");
+    }
+
+    // password validation
+    if (password === "") {
+        passwordInput.classList.add("form__label_invalid");
+    } else {
+        passwordInput.classList.remove("form__label_invalid");
+    }
+
+    // hide / show output
+    if (username === "" || password === "") {
+        output.classList.add("output_hidden");
+        loginForm.classList.remove("login-form_border");
+    } else {
+        output.classList.remove("output_hidden");
+        loginForm.classList.add("login-form_border");
+    }
+
+    uOut.innerText = username;
+    pOut.innerText = password;
+
+    return false;
+};
