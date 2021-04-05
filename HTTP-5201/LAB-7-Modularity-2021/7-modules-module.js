753b033749340968e63e817d6453540743a430da
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-05 10:42:51 -0400

    lab 7

--- HTTP-5201/LAB-7-Modularity-2021/7-modules-module.js
+++ HTTP-5201/LAB-7-Modularity-2021/7-modules-module.js
@@ -1,2 +1,16 @@
 //LAB 7 - MODULES - MODULE FILE
 //THIS FILE CONTAINS YOUR CUSTOM MODULE.
+
+var myModNS = (function () {
+  var myPrivateMsg = "Kyle created this myModNS module";
+
+  var citeMessage = function () {
+    alert(myPrivateMsg);
+  };
+
+  var module = {
+    citeMe: citeMessage
+  };
+
+  return module;
+})();
