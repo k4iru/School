4a01740f061577e260d08f782e6bcaeaf2070836
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-05 10:43:01 -0400

    lab7

--- HTTP-5201/LAB-7-Modularity-2021/7-modules-logic.js
+++ HTTP-5201/LAB-7-Modularity-2021/7-modules-logic.js
@@ -1,2 +1,19 @@
 //LAB 7 - MODULES - LOGIC FILE
 //THIS FILE CONTAINS ALL OF THE LOGIC THAT BELONGS WITH THE HTML PAGE - window.onload ETC. IN HERE YOU WILL REFERENCE AND USE YOUR MODULE.
+
+class NameTag extends HTMLElement {
+    constructor() {
+        super();
+        let template = document.querySelector("template");
+        let templateContent = template.content;
+        const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(templateContent.cloneNode(true));
+        this.onclick = function(){citeMe()};
+    }
+}
+
+customElements.define('name-tag', NameTag);
+
+function citeMe() {
+    //console.log("test");
+    myModNS.citeMe("test");
+}
