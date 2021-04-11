40979b96e4c9b6bb61b0c9170f07d13c81b83d21
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-10 23:37:21 -0400

    Update script.js

--- HTTP-5203/lab-7/scripts/script.js
+++ HTTP-5203/lab-7/scripts/script.js
@@ -1,8 +1,18 @@
 function initMap() {
   var humber = { lat: 43.7291338, lng: -79.6087013 };
-  let map = new google.maps.Map(document.getElementById("map"), {
-    center: humber,
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
+    center: humber,
   });
-  var marker = new google.maps.Marker({ position: humber, map: map });
+  const marker = new google.maps.Marker({
+    position: humber,
+    map,
+    title: "Click to zoom",
+  });
+
+  map.addListener("click", (e) => {
+    console.log(e.latLng);
+    map.setCenter(e.latLng);
+    marker.setPosition(e.latLng);
+  });
 }
