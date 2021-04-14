f92c0cb9f1e87eff2f12389cbcca740292bc5fb4
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-14 12:08:28 -0400

    finished lab 8

--- HTTP-5203/lab-8/index.php
+++ HTTP-5203/lab-8/index.php
@@ -6,10 +6,10 @@ $TRAKT = array(
   'client_id' => '67dba1a9114a86155cbcab18601fc6749698da487d37d3531b7e22849449b8c1',
   'client_secret' => '506a4a51413f80bf83405f12da0d6cf485b167f5c576dd285d6b1035dea313ef',
   'redirect_uri' => 'https://localhost/http-5203/lab-8/trakt.php',
-  'state' => 'awd7896gaysfhfh2454a' 
+  'state' => 'awd7896gaysfhfh2454a'
 );
 
-$url = TRAKT_URL . '/shows/popular';
+$url = TRAKT_URL . '/movies/trending?page=1&limit=15';
 
 /*
 I need these headers
@@ -33,12 +33,23 @@ $opts = array(
 $context = stream_context_create($opts);
 $result = json_decode(file_get_contents($url, false, $context));
 
-print "<ul>";
+print "
+<table>
+  <thead>
+    <tr>
+    <th>Movie Title</th>
+    <th>Year</th>
+    </tr>
+  </thead>
+  <tbody>";
+
+
 foreach ($result as $r) {
-  print "<li>$r->title</li>";
+  print "<tr>";
+  $movie = $r->movie;
+  $id = $movie->ids->imdb;
+  print "<td><a href='https://www.imdb.com/title/$id'>$movie->title</a></td>";
+  print "<td>$movie->year</td>";
+  print "</tr>";
 }
-print "</ul>";
-
-
-
-
+print "</tbody></table>";
