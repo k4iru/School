c8142847d2c2da9f90397f4d8ac2d8355aff36d2
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-28 00:54:44 -0400

    assignment 4

--- HTTP-5201/Assignment-4-Components-2021/A4-components-logic.js
+++ HTTP-5201/Assignment-4-Components-2021/A4-components-logic.js
@@ -1,3 +1,28 @@
 ﻿//ASSIGNMENT 4 WEB COMPONENTS
 //LOGIC FILE
 //IN THIS FILE YOU WILL SIMPLY GRAB YOUR CUSTOM ELEMENT AND ATTACH YOUR METHOD FROM YOUR MODULE TO IT.
+
+class HumberCountDown extends HTMLElement {
+  constructor() {
+    super();
+    let template = document.querySelector("template");
+    let templateContent = template.content;
+    const shadowRoot = this.attachShadow({ mode: "open" }).appendChild(
+      templateContent.cloneNode(true)
+    );
+  }
+  // once loaded get how many days left
+  connectedCallback() {
+    let daysLeft = this.shadowRoot.getElementById("days-left");
+    daysLeft.innerText = Semester.getDaysLeft();
+  }
+
+  // check days left if element moved
+  adoptedCallback() {
+    let daysLeft = this.shadowRoot.getElementById("days-left");
+    daysLeft.innerText = Semester.getDaysLeft();
+  }
+}
+
+// define the custom element
+customElements.define("humber-countdown", HumberCountDown);
--- HTTP-5201/Assignment-4-Components-2021/A4-components-module.js
+++ HTTP-5201/Assignment-4-Components-2021/A4-components-module.js
@@ -1,3 +1,25 @@
 ﻿//ASSIGNMENT 4 WEB COMPONENTS
 //MODULE FILE
 //IN THIS FILE YOU WILL CREATE YOUR CUSTOM MODULE FOR YOUR REQUIRED FUNCTIONALITY AND EXPOSE IT THROUGH A 'PUBLICLY' ACCESSIBLE METHOD.
+
+const Semester = (function () {
+
+  // April 30th, 2021, 17:00:00
+  const semesterEnd = new Date(2021, 3, 30, 17, 0, 0, 0);
+
+  var daysLeft = function () {
+    // today
+    let today = new Date();
+
+    // get difference in milliseconds. use math to get days left
+    let diff = Math.floor((semesterEnd - today) / (1000 * 60 * 60 * 24));
+    return diff;
+  };
+
+  // expose private methods
+  var module = {
+    getDaysLeft: daysLeft,
+  };
+
+  return module;
+})();
